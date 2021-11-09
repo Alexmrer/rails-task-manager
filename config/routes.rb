f849c4802feebff4f0589f6d2e0194ae03e7ce65
Alex Meurer <alxmrer@gmail.com>
2021-11-09 14:21:48 +0100

    View All, Create, Update, Delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'tasks', to: 'tasks#index'
+  # Create new task
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+  # Update task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+  # Show all
+  get 'tasks', to: 'tasks#index', as: :all_tasks
+  # Display one
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  # Delete
+  delete 'tasks/id', to: 'tasks#destroy'
 end
